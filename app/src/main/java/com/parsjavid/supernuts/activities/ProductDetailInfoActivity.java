162fca1e946342fcc16e518c11797fd2a4ec34af
ADMIN <ecompram@yahoo.com>
2019-01-16 19:57:49 +0330

    product detail info

--- app/src/main/java/com/parsjavid/supernuts/activities/ProductDetailInfoActivity.java
+++ app/src/main/java/com/parsjavid/supernuts/activities/ProductDetailInfoActivity.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.parsjavid.supernuts.Application;
 import com.parsjavid.supernuts.R;
 import com.parsjavid.supernuts.interfaces.ApiInterface;
 import com.parsjavid.supernuts.models.Product;
+import com.squareup.picasso.Picasso;
 
+import java.math.RoundingMode;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.inject.Inject;
@@ -28,6 +34,12 @@ public class ProductDetailInfoActivity extends AppCompatActivity {
     Retrofit retrofit;
     private ProgressBar progressBar;
     private Long productId;
+    private TextView productTitle;
+    private TextView productPrice;
+    private TextView productReview;
+    private TextView productProvderName;
+    private CardView productCardView;
+    private ImageView productImage;
 
 
     @Override
@@ -36,6 +48,11 @@ public class ProductDetailInfoActivity extends AppCompatActivity {
         setContentView(R.layout.product_details_info);
         Application.getmainComponent().Inject(this);
         progressBar=(ProgressBar)findViewById(R.id.progressBarInProductDetail);
+
+        productCardView=(CardView) findViewById(R.id.productCardView);
+        productTitle=(TextView) findViewById(R.id.productTitle);
+        productPrice=(TextView) findViewById(R.id.productPrice);
+
 
         Intent intent=getIntent();
         productId=intent.getLongExtra("productId",0);
@@ -50,6 +67,17 @@ public class ProductDetailInfoActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.GONE);
                 if(response.isSuccessful()) {
                     Product product = response.body();
+                    if(product!=null){
+                        productTitle.setText(product.getTitle());
+                        if(product.getPrice()!=null)
+                            productPrice.setText(String.format(Locale.forLanguageTag("fa-IR"), "%,.2f", product.getPrice().setScale(2, RoundingMode.DOWN)));
+                        if (product.getBaseImageFilePath600() != null && product.getBaseImageFilePath600().trim() != "")
+                            Picasso.get()
+                                    .load(product.getBaseImageFilePath600())
+                                    .centerCrop()
+                                    .into(productImage);
+
+                    }
                 }
             }
 
